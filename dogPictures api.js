dd387ad053851a3cd45675bcac87aaaeeb183c22
Anjali singh <singhanjali57335@gmail.com>
2026-09-20 17:37:59 +0530

    Add files via upload

---
+++ dogPictures api.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector("button");
+let url2 = "https://dog.ceo/api/breeds/image/random"
+
+btn.addEventListener("click",async () =>{
+    let link = await getImage();
+    let img = document.querySelector("#result");
+    img.setAttribute("src", link);
+    console.log(link);
+});
+
+async function getImage() {
+    try{
+        let res = await axios.get(url2);
+        return res.data.message;
+    }
+    catch (e){
+console.log("error -", e);
+return "/";
+    }
+}
